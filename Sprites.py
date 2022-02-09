27b06fcf49af181bc61dda158052360c4cca6f3e
ElArquitectorgo <mega2814s@gmail.com>
2022-02-09 15:36:55 +0100

    better

--- Sprites.py
+++ Sprites.py
@@ -23,10 +23,10 @@ class Pawn(Piece):
         self.name = "P"
         self.promoted = [False, None, None]
 
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_pawn_w
             self.value = 10
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_pawn_b
             self.value = -10
 
@@ -52,15 +52,15 @@ class Pawn(Piece):
                 return Bishop.get_valid_moves(self, x, y)
 
         self.valid_moves = list()
-        if (self.color == "WHITE"):
-            if (y - 1 >= 0 and self.game.tablero[x][y - 1] == ""):
-                if (y - 1 >= 0):
+        if self.color == "WHITE":
+            if y - 1 >= 0 and self.game.tablero[x][y - 1] == "":
+                if y - 1 >= 0:
                     self.valid_moves.append((x, y - 1))
-                if (y == 6 and self.game.tablero[x][y - 2] == ""):
+                if y == 6 and self.game.tablero[x][y - 2] == "":
                     self.valid_moves.append((x, y - 2))
-            if (y - 1 >= 0 and x - 1 >= 0 and not self.game.tablero[x - 1][y - 1] == ""):
+            if y - 1 >= 0 and x - 1 >= 0 and not self.game.tablero[x - 1][y - 1] == "":
                 self.valid_moves.append((x - 1, y - 1))
-            if (y - 1 >= 0 and x + 1 < 8 and not self.game.tablero[x + 1][y - 1] == ""):
+            if y - 1 >= 0 and x + 1 < 8 and not self.game.tablero[x + 1][y - 1] == "":
                 self.valid_moves.append((x + 1, y - 1))
             # Al paso
             if self.y == 3:
@@ -75,14 +75,14 @@ class Pawn(Piece):
                         if self.is_comible(sprite):
                             self.valid_moves.append((x + 1, y - 1))
         else:
-            if (y + 1 < 8 and self.game.tablero[x][y + 1] == ""):
-                if (y + 1 < 8):
+            if y + 1 < 8 and self.game.tablero[x][y + 1] == "":
+                if y + 1 < 8:
                     self.valid_moves.append((x, y + 1))
-                if (y == 1 and self.game.tablero[x][y + 2] == ""):
+                if y == 1 and self.game.tablero[x][y + 2] == "":
                     self.valid_moves.append((x, y + 2))
-            if (y + 1 < 8 and x - 1 >= 0 and not self.game.tablero[x - 1][y + 1] == ""):
+            if y + 1 < 8 and x - 1 >= 0 and not self.game.tablero[x - 1][y + 1] == "":
                 self.valid_moves.append((x - 1, y + 1))
-            if (y + 1 < 8 and x + 1 < 8 and not self.game.tablero[x + 1][y + 1] == ""):
+            if y + 1 < 8 and x + 1 < 8 and not self.game.tablero[x + 1][y + 1] == "":
                 self.valid_moves.append((x + 1, y + 1))
             # Al paso
             if self.y == 4:
@@ -103,10 +103,10 @@ class Bishop(Piece):
     def __init__(self, game, color, x, y):
         super().__init__(game, color, x, y)
         self.name = "B"
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_bishop_w
             self.value = 30
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_bishop_b
             self.value = -30
 
@@ -115,38 +115,38 @@ class Bishop(Piece):
         #Abajo derecha
         i = x + 1
         j = y + 1
-        while (i < 8 and j < 8 and self.game.tablero[i][j] == ""):
+        while i < 8 and j < 8 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i += 1
             j +=1
-        if (i < 8 and j < 8 and not self.game.is_same_color(self.color, i, j)):
+        if i < 8 and j < 8 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Abajo izquierda
         i = x - 1
         j = y + 1;
-        while (i > 0 and j < 8 and self.game.tablero[i][j] == ""):
+        while i > 0 and j < 8 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i -= 1
             j += 1
-        if (i >= 0 and j < 8 and not self.game.is_same_color(self.color, i, j)):
+        if i >= 0 and j < 8 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Arriba derecha
         i = x + 1
         j = y - 1
-        while (i < 8 and j > 0 and self.game.tablero[i][j] == ""):
+        while i < 8 and j > 0 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i += 1
             j -= 1
-        if (i < 8 and j >= 0 and not self.game.is_same_color(self.color, i, j)):
+        if i < 8 and j >= 0 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Arriba izquierda
         i = x - 1 
         j = y - 1
-        while (i > 0 and j > 0 and self.game.tablero[i][j] == ""):
+        while i > 0 and j > 0 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i -= 1 
             j -= 1
-        if (i >= 0 and j >= 0 and not self.game.is_same_color(self.color, i, j)):
+        if i >= 0 and j >= 0 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         return self.valid_moves
 
@@ -154,34 +154,34 @@ class Knight(Piece):
     def __init__(self, game, color, x, y):
         super().__init__(game, color, x, y)
         self.name = "N"
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_knight_w
             self.value = 30
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_knight_b
             self.value = -30
 
     def get_valid_moves(self, x, y):
         self.valid_moves = list()
         #Arriba izquierda
-        if (x - 1 >= 0 and y - 2 >= 0):
+        if x - 1 >= 0 and y - 2 >= 0:
             self.valid_moves.append((x - 1, y - 2))
-        if (x - 2 >= 0 and y - 1 >= 0):
+        if x - 2 >= 0 and y - 1 >= 0:
             self.valid_moves.append((x - 2, y - 1))
         #Arriba derecha
-        if (x + 1 < 8 and y - 2 >= 0):
+        if x + 1 < 8 and y - 2 >= 0:
             self.valid_moves.append((x + 1, y - 2))
-        if (x + 2 < 8 and y - 1 >= 0):
+        if x + 2 < 8 and y - 1 >= 0:
             self.valid_moves.append((x + 2, y - 1))
         #Abajo izquierda
-        if (x - 1 >= 0 and y + 2 < 8):
+        if x - 1 >= 0 and y + 2 < 8:
             self.valid_moves.append((x - 1, y + 2))
-        if (x - 2 >= 0 and y + 1 < 8):
+        if x - 2 >= 0 and y + 1 < 8:
             self.valid_moves.append((x - 2, y + 1))
         #Abajo derecha
-        if (x + 1 < 8 and y + 2 < 8):
+        if x + 1 < 8 and y + 2 < 8:
             self.valid_moves.append((x + 1, y + 2))
-        if (x + 2 < 8 and y + 1 < 8):
+        if x + 2 < 8 and y + 1 < 8:
             self.valid_moves.append((x + 2, y + 1))
         return self.game.get_real_valid_moves(self.color, self.valid_moves)
 
@@ -190,10 +190,10 @@ class Rook(Piece):
         super().__init__(game, color, x, y)
         self.name = "R"
         self.castling = True
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_rook_w
             self.value = 50
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_rook_b
             self.value = -50
 
@@ -206,24 +206,24 @@ class Rook(Piece):
         r = False; d = False
         
         for i in range(8):
-            if (self.game.tablero[i][y] != ""):
-                if (i < x):
+            if self.game.tablero[i][y] != "":
+                if i < x:
                     self.left_closer_piece = (i, y)
-                elif (i > x and not r):
+                elif i > x and not r:
                     self.right_closer_piece = (i, y)
                     r = True
-            if (self.game.tablero[x][i] != ""):
-                if (i < y):
+            if self.game.tablero[x][i] != "":
+                if i < y:
                     self.up_closer_piece = (x, i)
-                elif (i > y and not d):
+                elif i > y and not d:
                     self.down_closer_piece = (x, i)
                     d = True
 
         for i in range(self.left_closer_piece[0], self.right_closer_piece[0] + 1):
-            if (i != x and not self.game.is_same_color(self.color, i, y)):
+            if i != x and not self.game.is_same_color(self.color, i, y):
                 self.valid_moves.append((i, y))
         for j in range(self.up_closer_piece[1], self.down_closer_piece[1] + 1):
-            if (j != y and not self.game.is_same_color(self.color, x, j)):
+            if j != y and not self.game.is_same_color(self.color, x, j):
                 self.valid_moves.append((x, j))
         #self.valid_moves = self.game.getRealValidMoves(self.color, self.valid_moves)
         return self.valid_moves
@@ -233,33 +233,33 @@ class King(Piece):
         super().__init__(game, color, x, y)
         self.name = "K"
         self.castling_turn = None
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_king_w
             self.value = 900
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_king_b
             self.value = -900
 
     def get_valid_moves(self, x, y):
         self.valid_moves = list()
         #Arribas
-        if (x - 1 >= 0 and y - 1 >= 0):
+        if x - 1 >= 0 and y - 1 >= 0:
             self.valid_moves.append((x - 1, y - 1))
-        if (y - 1 >= 0):
+        if y - 1 >= 0:
             self.valid_moves.append((x, y - 1))
-        if (x + 1 < 8 and y - 1 >= 0):
+        if x + 1 < 8 and y - 1 >= 0:
             self.valid_moves.append((x + 1, y - 1))
         #Abajos
-        if (x - 1 >= 0 and y + 1 < 8):
+        if x - 1 >= 0 and y + 1 < 8:
             self.valid_moves.append((x - 1, y + 1))
-        if (y + 1 < 8):
+        if y + 1 < 8:
             self.valid_moves.append((x, y + 1))
-        if (x + 1 < 8 and y + 1 < 8):
+        if x + 1 < 8 and y + 1 < 8:
             self.valid_moves.append((x + 1, y + 1))
         #Horizontal
-        if (x - 1 >= 0):
+        if x - 1 >= 0:
             self.valid_moves.append((x - 1, y))
-        if (x + 1 < 8):
+        if x + 1 < 8:
             self.valid_moves.append((x + 1, y))
         #Enroque
         if self.castling_turn is None:
@@ -286,10 +286,10 @@ class Queen(Piece):
     def __init__(self, game, color, x, y):
         super().__init__(game, color, x, y)
         self.name = "Q"
-        if (color == "WHITE"):
+        if color == "WHITE":
             self.image = game.img_queen_w
             self.value = 90
-        if (color == "BLACK"):
+        if color == "BLACK":
             self.image = game.img_queen_b
             self.value = -90
 
@@ -302,60 +302,60 @@ class Queen(Piece):
         r = False; d = False
         
         for i in range(8):
-            if (self.game.tablero[i][y] != ""):
-                if (i < x):
+            if self.game.tablero[i][y] != "":
+                if i < x:
                     self.left_closer_piece = (i, y)
-                elif (i > x and not r):
+                elif i > x and not r:
                     self.right_closer_piece = (i, y)
                     r = True
-            if (self.game.tablero[x][i] != ""):
-                if (i < y):
+            if self.game.tablero[x][i] != "":
+                if i < y:
                     self.up_closer_piece = (x, i)
-                elif (i > y and not d):
+                elif i > y and not d:
                     self.down_closer_piece = (x, i)
                     d = True
 
         for i in range(self.left_closer_piece[0], self.right_closer_piece[0] + 1):
-            if (i != x and not self.game.is_same_color(self.color, i, y)):
+            if i != x and not self.game.is_same_color(self.color, i, y):
                 self.valid_moves.append((i, y))
         for j in range(self.up_closer_piece[1], self.down_closer_piece[1] + 1):
-            if (j != y and not self.game.is_same_color(self.color, x, j)):
+            if j != y and not self.game.is_same_color(self.color, x, j):
                 self.valid_moves.append((x, j))
         #Abajo derecha
         i = x + 1
         j = y + 1
-        while (i < 8 and j < 8 and self.game.tablero[i][j] == ""):
+        while i < 8 and j < 8 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i += 1
             j +=1
-        if (i < 8 and j < 8 and not self.game.is_same_color(self.color, i, j)):
+        if i < 8 and j < 8 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Abajo izquierda
         i = x - 1
         j = y + 1;
-        while (i > 0 and j < 8 and self.game.tablero[i][j] == ""):
+        while i > 0 and j < 8 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i -= 1
             j += 1
-        if (i >= 0 and j < 8 and not self.game.is_same_color(self.color, i, j)):
+        if i >= 0 and j < 8 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Arriba derecha
         i = x + 1
         j = y - 1
-        while (i < 8 and j > 0 and self.game.tablero[i][j] == ""):
+        while i < 8 and j > 0 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i += 1
             j -= 1
-        if (i < 8 and j >= 0 and not self.game.is_same_color(self.color, i, j)):
+        if i < 8 and j >= 0 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         #Arriba izquierda
         i = x - 1 
         j = y - 1
-        while (i > 0 and j > 0 and self.game.tablero[i][j] == ""):
+        while i > 0 and j > 0 and self.game.tablero[i][j] == "":
             self.valid_moves.append((i, j))
             i -= 1 
             j -= 1
-        if (i >= 0 and j >= 0 and not self.game.is_same_color(self.color, i, j)):
+        if i >= 0 and j >= 0 and not self.game.is_same_color(self.color, i, j):
             self.valid_moves.append((i, j))
         return self.valid_moves
         
