5a9e17c93fdd316118247f730c7ec02d94e9bd1e
ElArquitectorgo <mega2814s@gmail.com>
2022-03-04 11:55:53 +0100

    FEN

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ class Chess(Game):
         position5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
         position6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
 
-
         tablero = [["" for i in range(8)] for j in range(8)]
         str = (position3).split("/")
         for i in range(8):
