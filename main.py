bad550e8e761dfd537905ab33c7b18bd7bc4b06f
ElArquitectorgo <mega2814s@gmail.com>
2022-03-04 12:55:34 +0100

    FEN

--- main.py
+++ main.py
@@ -48,17 +48,23 @@ class Chess(Game):
 
         position1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         position2 = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"
-        position3 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - "
+        position3 = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"
         position4 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
         position5 = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"
         position6 = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"
 
         tablero = [["" for i in range(8)] for j in range(8)]
-        str = (position3).split("/")
+        str = (position2).split("/")
         for i in range(8):
             data = str[i]
             if i == 7:
-                data = str[i].split()[0]
+                data = str[i].split()
+                if data[1] == "w":
+                    self.turn = 1
+                else:
+                    self.turn = 2
+
+                data = data[0]
 
             cnt = 0
             for c in data:
